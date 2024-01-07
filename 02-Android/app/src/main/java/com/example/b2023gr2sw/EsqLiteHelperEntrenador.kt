d9a416f79bb94e84208bfeacc062ecb1e953ecd6
Victor <victor.moreno01@epn.edu.ec>
2024-01-07 13:40:18 -0500

    a

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/EsqLiteHelperEntrenador.kt
+++
@@ -1,4 +0,0 @@
-package com.example.b2023gr2sw
-
-class EsqLiteHelperEntrenador {
-}
--- 02-Android/app/src/main/java/com/example/b2023gr2sw/ui/theme/BEntrenador.kt
+++
@@ -1,8 +0,0 @@
-package com.example.b2023gr2sw.ui.theme
-
-class BEntrenador(
-    var id: Int,
-    var nombre: String?,
-    var descripcion: String?
-) {
-}
