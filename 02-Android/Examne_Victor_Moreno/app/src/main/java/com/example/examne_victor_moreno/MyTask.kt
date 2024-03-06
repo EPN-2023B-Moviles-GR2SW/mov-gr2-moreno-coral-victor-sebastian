199c4e7a08e093f0c658a09ed164f7977b315245
Victor <victor.moreno01@epn.edu.ec>
2024-03-05 23:50:51 -0500

    a

---
+++ 02-Android/Examne_Victor_Moreno/app/src/main/java/com/example/examne_victor_moreno/MyTask.kt
@@ -0,0 +1,33 @@
+import com.google.firebase.Timestamp
+
+data class Task(
+    var id: String? = null,
+    var title: String = "",
+    var description: String = "",
+    var dueDate: Timestamp? = null,
+    var priority: String = ""
+) {
+
+    companion object {
+        const val COLLECTION_NAME = "tasks"
+
+        fun fromMap(id: String, data: Map<String, Any?>): Task {
+            return Task(
+                id,
+                data["title"] as? String ?: "",
+                data["description"] as? String ?: "",
+                data["dueDate"] as? Timestamp,
+                data["priority"] as? String ?: ""
+            )
+        }
+    }
+
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "title" to title,
+            "description" to description,
+            "dueDate" to dueDate,
+            "priority" to priority
+        )
+    }
+}
